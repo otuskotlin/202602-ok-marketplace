680bbb068d5ff6f5cbfe0845f43c81031f8fe1b4
Сергей Окатов <sokatov@gmail.com>
2026-02-26 18:08:50 +0500

    обновление проекта

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,10 +5,7 @@ pluginManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-rootProject.name = "otuskotlin-marketplace-202511"
+rootProject.name = "otuskotlin-marketplace-202602"
 
 include("m1l1-first")
 
