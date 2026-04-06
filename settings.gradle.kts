0c81f8cea4abc151a8b17c404115eec2182fea92
enji8 <q@w.r>
2026-04-06 20:05:39 +0300

    m2l6 - move to lessons

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,15 +7,4 @@ pluginManagement {
 
 rootProject.name = "otuskotlin-marketplace-202602"
 
-include("m1l1-first")
-include("m1l2-basic")
-include("m1l3-func")
-include("m1l4-oop")
-include("m2l1-dsl")
-
-include("m2l2-coroutines")
-include("m2l3-flows")
-include("m2l4-kmp")
-include("m2l5-1-interop")
-include("m2l5-2-j")
-include("m2l6-gradle")
+includeBuild("lessons")
