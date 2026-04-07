cccb13c2ceca24fb8569d75f0276040224c17500
evgnep <enji81@gmail.com>
2026-04-07 06:45:57 +0300

    M2l6 gradle (#10)
    * m2l6 - tasks demo
    
    * m2l6 - move to lessons
    
    * m2l6 - toml
    
    * m2l6 - plugin
    
    ---------
    
    Co-authored-by: enji8 <q@w.r>

---
+++ lessons/m2l6-gradle/src/jvmMain/kotlin/RustExample.kt
@@ -0,0 +1,13 @@
+package ru.otus.otuskotlin.interop
+
+class RustExample {
+    external fun rust_add(a: Int, b: Int): Int
+
+    init {
+        System.loadLibrary("c_jni")
+    }
+}
+
+fun main() {
+    println(my.x.MY_VERSION)
+}
