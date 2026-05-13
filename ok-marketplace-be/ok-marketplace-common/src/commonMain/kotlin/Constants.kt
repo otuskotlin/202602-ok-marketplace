4a10764780ed70b27bbd64c1fb058d52c04235c8
sproshchaev <sproshchaev@gmail.com>
2026-05-13 11:03:37 +0500

    fix: M4L2 models
    (cherry picked from commit b97de39989f64434d7be0326f97d8e4b2810500d)

--- ok-marketplace-be/ok-marketplace-common/src/commonMain/kotlin/Constants.kt
+++ ok-marketplace-be/ok-marketplace-common/src/commonMain/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 package ru.otus.otuskotlin.marketplace.common
 
-import kotlinx.datetime.Instant
+import kotlin.time.Instant
 
 private val INSTANT_NONE = Instant.fromEpochMilliseconds(Long.MIN_VALUE)
 val Instant.Companion.NONE
