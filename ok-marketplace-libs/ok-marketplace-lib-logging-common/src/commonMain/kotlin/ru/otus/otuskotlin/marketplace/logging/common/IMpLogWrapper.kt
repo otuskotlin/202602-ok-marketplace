c5d226bfc2bd3c1f7061438a982099c3157c6dc4
sproshchaev <sproshchaev@gmail.com>
2026-05-13 10:49:40 +0500

    fix: M4L2 models
    (cherry picked from commit b97de39989f64434d7be0326f97d8e4b2810500d)

--- ok-marketplace-libs/ok-marketplace-lib-logging-common/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/logging/common/IMpLogWrapper.kt
+++ ok-marketplace-libs/ok-marketplace-lib-logging-common/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/logging/common/IMpLogWrapper.kt
@@ -1,6 +1,6 @@
 package ru.otus.otuskotlin.marketplace.logging.common
 
-import kotlinx.datetime.Clock
+import kotlin.time.Clock
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
